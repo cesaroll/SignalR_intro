a2263b3cdc2c9abcc10f414a1ec85d45696d1197
Cesar Lopez <cesarl@outlook.com>
2016-02-03 10:36:30 -0700

    SignalR Connect, Reconnect, Disconnect

--- SR/Conn/MyEndPoint.cs
+++ SR/Conn/MyEndPoint.cs
@@ -9,6 +9,21 @@ namespace SR.Conn
 {
     public class MyEndPoint : PersistentConnection
     {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            return Connection.Broadcast("Connection " + connectionId + "<span style='color: blue'> connected.</span>");
+        }
+
+        protected override Task OnReconnected(IRequest request, string connectionId)
+        {
+            return Connection.Broadcast("Connection " + connectionId + "<span style='color: green'> reconnected.</span>");
+        }
+
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            return Connection.Broadcast("Connection " + connectionId + "<span style='color: red'> disconnected.</span>");
+        }
+
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
             //Broadcast to all awaiting clients (connections)
