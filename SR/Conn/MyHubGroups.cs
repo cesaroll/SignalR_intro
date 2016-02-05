05d4535382220132bf402d7159336862a7972a40
Cesar Lopez <cesarl@outlook.com>
2016-02-05 16:58:50 -0700

    Joinning/Leaving several groups

--- SR/Conn/MyHubGroups.cs
+++ SR/Conn/MyHubGroups.cs
@@ -11,23 +11,32 @@ namespace SR.Conn
     [HubName("MyHubGroups")]
     public class MyHubGroups : Hub
     {
-        
+        public override Task OnConnected()
+        {
+            // Specific to the connection
+            this.Clients.Caller.addMessage("<span style='color: blue'>Connected: </span>" + Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
         [HubMethodName("JoinGroup")]
         public Task JoinGroup(string groupName)
         {
+            this.Clients.Caller.addMessage("<span style='color: green'>Joinning group: </span>" + groupName);
             return Groups.Add(Context.ConnectionId, groupName);
         }
         
         [HubMethodName("LeaveGroup")]
         public Task LeaveGroup(string groupName)
         {
+            this.Clients.Caller.addMessage("<span style='color: orange'>Leaving group: </span>" + groupName);
             return Groups.Remove(Context.ConnectionId, groupName);
         }
         
         [HubMethodName("Send")]
         public Task Send(string groupName, string msg)
         {
-            return Clients.Group(groupName).addMessage("<span style='color: green'>All: </span>" + msg);
+            return Clients.Group(groupName).addMessage("<span style='color: green'>" + groupName + ": </span>" + msg);
         }
         
         
