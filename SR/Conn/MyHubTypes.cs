3a3e5e5d397594d4a36603a07755f77cceb2aba3
Cesar Lopez <cesarl@outlook.com>
2016-02-05 17:07:07 -0700

    Demo return types

---
+++ SR/Conn/MyHubTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SR.Conn
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+    [HubName("MyHubTypes")]
+    public class MyHubTypes : Hub
+    {
+        [HubMethodName("Send")]
+        public Product Send(string data)
+        {
+            return new Product { Name = "Pepsi", CategoryId = 1 };
+        }
+    }
+}
